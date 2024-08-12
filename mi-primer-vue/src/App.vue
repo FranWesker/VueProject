ceea458e074af8d9089fb9a3a899434a0669d028
Fran <javierfran2409@gmail.com>
2024-08-12 10:54:40 +0200

    Modificaciones en el App.vue ( - Viendo las constantes de vue y modificando varias cositas y jugando con los arrays los v-for, v-if, v-else)

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,9 +1,42 @@
 <script setup>
+ // Creación de constante
+ const name = 'Vue Dinamico';
 
+ // Creación de constantes para los estilos
+ const stylecolor = "color: blue";
+
+ // Constante de array de colores
+ const arraycolor = ["blue", "green", "red"];
+
+ // Array de frutas
+ const arrayFrutas = ["🍎","🍏","🍅","🍇","🍈"];
+
+ // Constante Booleana
+ const activo = true;
 </script>
 
 <template>
-  <h1>Hola Mundo</h1>
+  <center>
+    <h1>Hola Mundo {{name.toUpperCase()}}</h1>
+    <br>
+    <h2 v-bind:style="stylecolor">Soy de color azul</h2>
+    <br>
+    <h2 :style="'color: ${arraycolor[2]}'">Soy batman</h2>
+    <br>
+    <h2 v-if="activo">Estoy activo</h2>
+    <p v-else>Estoy inactivo</p>
+
+    <h2>{{arrayFrutas}}</h2>
+    <br>
+    <h1 style="text-decoration: underline">Ordenación de Frutas</h1>
+    <ul>
+      <li
+        v-for="(fruta, index) in arrayFrutas">
+        {{ index+1 }} - {{(fruta)}}
+      </li>
+    </ul>
+  </center>
+
 </template>
 
 <style>
